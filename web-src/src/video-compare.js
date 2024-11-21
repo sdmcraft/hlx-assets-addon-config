4cefbe50227491885d93b782a0e1d52973e6a5af
Satya Deep Maheshwari <satyam@adobe.com>
2024-11-21 22:38:45 +0530

    wip

--- web-src/src/video-compare.js
+++ web-src/src/video-compare.js
@@ -1,56 +1,70 @@
 
 function getDeviceSpecificVideoUrl(videoUrl) {
-    const { userAgent } = navigator;
-    const isIOS = /iPad|iPhone|iPod/.test(userAgent);
-    const isSafari = (/Safari/i).test(userAgent) && !(/Chrome/i).test(userAgent) && !(/CriOs/i).test(userAgent) && !(/Android/i).test(userAgent) && !(/Edg/i).test(userAgent);
+    // const { userAgent } = navigator;
+    // const isIOS = /iPad|iPhone|iPod/.test(userAgent);
+    // const isSafari = (/Safari/i).test(userAgent) && !(/Chrome/i).test(userAgent) && !(/CriOs/i).test(userAgent) && !(/Android/i).test(userAgent) && !(/Edg/i).test(userAgent);
 
-    const manifest = (isIOS || isSafari) ? 'manifest.m3u8' : 'manifest.mpd';
+    // const manifest = (isIOS || isSafari) ? 'manifest.m3u8' : 'manifest.mpd';
+    const manifest = 'manifest.mpd'; // Always use DASH for now
     return videoUrl.replace(/manifest\.mpd|manifest\.m3u8|play/, manifest);
 }
 
-function createVideoJSElement(manifestUrl) {
-    const videojsManifestPlayer = document.createElement('video-js');
-    const rand = Math.floor(Math.random() * 1000);
-    videojsManifestPlayer.id = `videojs-manifest-player-${rand}`;
-    const source = document.createElement('source');
-    source.setAttribute('src', manifestUrl);
-    videojsManifestPlayer.appendChild(source);
-    return videojsManifestPlayer;
-}
-
-document.getElementById('playButton').addEventListener('click', () => {
-
+function setupVJS(manifestUrl) {
     if (document.getElementsByTagName('video-js').length > 0) {
         document.getElementsByTagName('video-js')[0].remove();
     }
-    if (document.getElementsByTagName('video').length > 0) {
-        document.getElementsByTagName('video')[0].remove();
-    }
-
-
+    const videojsManifestPlayer = document.createElement('video-js');
+    const rand = Math.floor(Math.random() * 1000);
+    videojsManifestPlayer.id = `videojs-manifest-player-${rand}`;
+    const manifestPlayerPanel = document.getElementById('manifestPlayerPanel');
     manifestPlayerPanel.appendChild(videojsManifestPlayer);
     const videojsManifestPlayerObj = videojs(`videojs-manifest-player-${rand}`, {
         controls: true,
         autoplay: false,
         preload: 'auto',
+        muted: true,
+        html5: {
+            dash: {
+                limitRenditionByPlayerDimensions: true
+            }
+        }
     });
 
+    videojsManifestPlayerObj.ready(function() {
+        videojsManifestPlayerObj.src({
+            src: manifestUrl,
+            type: 'application/dash+xml'
+        });
+    });
+    videojsManifestPlayerObj.on('loadedmetadata', function() {
+        var tracks = videojsManifestPlayerObj.dash.mediaPlayer.getTracksFor('video');
+        var bestTrack = tracks.reduce((prev, current) => (prev.bandwidth > current.bandwidth) ? prev : current);
+        if (bestTrack) {
+            videojsManifestPlayerObj.dash.mediaPlayer.setInitialMediaSettingsFor('video', {
+                id: bestTrack.id
+            });
+        }
+    });
 
+    return videojsManifestPlayerObj;
+}
+
+function setupProgressivePlayer(mp4Url) {
+    if (document.getElementsByTagName('video').length > 0) {
+        document.getElementsByTagName('video')[0].remove();
+    }
     const progressivePlayer = document.createElement('video');
     progressivePlayer.controls = true;
     progressivePlayer.preload = 'auto';
-    const source2 = document.createElement('source');
-    source2.setAttribute('src', document.getElementById('mp4Url').value);
-    source2.setAttribute('type', 'video/mp4');
-    progressivePlayer.appendChild(source2);
+    progressivePlayer.muted = true;
+    const source = document.createElement('source');
+    source.setAttribute('src', mp4Url);
+    source.setAttribute('type', 'video/mp4');
+    progressivePlayer.appendChild(source);
+    const progressivePlayerPanel = document.getElementById('progressivePlayerPanel');
     progressivePlayerPanel.appendChild(progressivePlayer);
-
-    setTimeout(() => {
-        videojsManifestPlayerObj.play();
-        progressivePlayer.play();
-    }, 1000);
-});
-
+    return progressivePlayer;
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     // Read video urls from request params
@@ -63,13 +77,14 @@ window.addEventListener('DOMContentLoaded', () => {
     if (mp4UrlParam) {
         document.getElementById('mp4Url').value = mp4UrlParam;
     }
-    const manifestPlayerPanel = document.getElementById('manifestPlayerPanel');
-    const progressivePlayerPanel = document.getElementById('progressivePlayerPanel');
-    const manifestUrl = getDeviceSpecificVideoUrl(document.getElementById('manifestUrl').value);
-    const playerType = document.getElementById('playerType').value;
-    if(playerType === 'video-js') {
-        const videojsManifestPlayer = createVideoJSElement(manifestUrl);
-        manifestPlayerPanel.appendChild(videojsManifestPlayer);
-    } else {
-    }
+
+    document.getElementById('playButton').addEventListener('click', () => {
+        const progressivePlayer = setupProgressivePlayer(document.getElementById('mp4Url').value);
+        const videojsManifestPlayerObj = setupVJS(getDeviceSpecificVideoUrl(document.getElementById('manifestUrl').value));
+        setTimeout(() => {
+            videojsManifestPlayerObj.play();
+            progressivePlayer.play();
+        }, 1000);
+    });
+
 });
